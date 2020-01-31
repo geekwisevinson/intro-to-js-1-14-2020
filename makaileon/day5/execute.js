5591c97e843de7881c73aafa7d8dcc4f60e87325
OG Macko <Macko@Apples-MacBook-Pro.local>
2020-01-30 18:44:21 -0800

    day5

---
+++ makaileon/day5/execute.js
@@ -0,0 +1,8 @@
+console.log(primaryColor, appName);
+createHeader();
+updateScore();
+const player = new Player({name: 'player 1', life: 100});
+
+const player2 = new Player({name: 'player 2', life: 99});
+
+const player3 = new Player({name: 'player 3', life: 98});
