3f7dad1fddc23a6af69182fcc8da9259ca10e594
OG Macko <Macko@Apples-MacBook-Pro.local>
2020-02-11 20:56:37 -0800

    continuing asteroids

--- makaileon/asteroid/spaceship.js
+++ makaileon/asteroid/spaceship.js
@@ -1,12 +1,13 @@
 class Spaceship {
     life = 100;
     name = 'player 1';
-
+    ySpeed = 0;
+    xSpeed = 0;
+    
     constructor({life, name}) {
         this.name = name;
         this.life = life;
         this.append();
         this.el.style.position = 'absolute';
-    }
-
+    };
 }
